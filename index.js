0e65163857059494071a0be812d2837175c9b6a8
saifxj1 <saifyaset@gmail.com>
2025-02-28 20:38:38 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('guildMemberAdd', async (member) => {
+    try {
+        await member.send(`Hey ${member.user.username} ! Welcome to Falcon Discord server! Please read through the https://discord.com/channels/985679788020748328/1233215516273934440 before using the server, Enjoy your stay! 
+اهلاََ ${member.user.username}  ! مرحبا بك في سيرفر الدسكورد الرسمي الخاص ببندريتا اكس, يرجى قراءة جميع القوانين https://discord.com/channels/985679788020748328/1233215516273934440 قبل استخدام السيرفر, استمتع.`);
+        console.log(`تم إرسال رسالة ترحيب لـ ${member.user.tag}`);
+    } catch (error) {
+        console.error(`تعذر إرسال الرسالة لـ ${member.user.tag}:`, error);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('البوت يعمل!');
+});
+
+app.listen(PORT, () => console.log(`السيرفر شغال على المنفذ ${PORT}`));
+
+client.login(process.env.TOKEN);
